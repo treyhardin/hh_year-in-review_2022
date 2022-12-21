a830f689e52e1b2618bf428cb1102545d33a721b
Trey Hardin <treyhardindesign@gmail.com>
2022-12-21 09:44:19 -0500

    Add Rollup Options

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,11 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    rollupOptions: {
+      external: [
+        "ScrollSmoother", // ignore react stuff
+      ],
+    },
   },
+    
 });
